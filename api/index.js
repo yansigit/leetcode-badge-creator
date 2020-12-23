11974945f7a61ad4ebe633d09fe73321e7544b01
gem <44089734+yansigit@users.noreply.github.com>
2020-12-23 20:52:26 +0900

    bug fixed 1

--- api/index.js
+++ api/index.js
@@ -6,14 +6,41 @@ const badge_filepath = path.join(__dirname, "badge.png")
 const error_filepath = path.join(__dirname, "no-profile-available.png")
 
 const getBadge = async (username) => {
-    const puppeteer = require('puppeteer');
 
     if (fs.existsSync(badge_filepath)) {
         fs.unlinkSync(badge_filepath);
     }
 
+    let chromium = {};
+    let puppeteer;
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        // running on the Vercel platform.
+        chromium = require('chrome-aws-lambda');
+        puppeteer = require('puppeteer-core');
+    } else {
+        // running locally.
+        puppeteer = require('puppeteer');
+    }
+
     return await (async () => {
-        const browser = await puppeteer.launch();
+        let browser;
+        try {
+            if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+                browser = await puppeteer.launch({
+                    args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+                    defaultViewport: chromium.defaultViewport,
+                    executablePath: await chromium.executablePath,
+                    headless: true,
+                    ignoreHTTPSErrors: true,
+                });
+            } else {
+                browser = await puppeteer.launch();
+            }
+        } catch (err) {
+            console.error(err);
+            return null;
+        }
+
         const page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4230.1 Safari/537.36');
 
@@ -44,8 +71,8 @@ app.get('/api/leetcode/:username', async (req, res) => {
     }
 });
 
-module.exports = app;
+// module.exports = app;
 
-// app.listen(80, () => {
-//     console.log(`Example app listening at http://localhost`)
-// })
+app.listen(80, () => {
+    console.log(`Example app listening at http://localhost`)
+})
